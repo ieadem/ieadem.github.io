7d0cb96907ab896b73e0429ec72a76bf7f2e8663
sa-mods <samodsoficial@gmail.com>
2021-12-22 19:53:04 -0300

    initial commit

--- js/data.js
+++ js/data.js
@@ -1,3 +1,61 @@
+const adms = {
+	"sede": {
+		"image": "img/icon.png",
+		"name": "Adm Sede",
+		"text": "Bairro: centro<br/>rua: Jovita fontes<br/>n⁰: 443"
+	},
+	"califórnia": {
+		"image": "",
+		"name": "Adm Califórnia",
+		"text": "bairro: jardim califórnia<br/>rua: ...<br/>n⁰: ..."
+	},
+	"brisas do mar": {
+		"image": "",
+		"name": "Adm Brisas do mar",
+		"text": "bairro: brisas do mar<br/>rua: ...<br/>n⁰: ..."
+	},
+	"beira rio": {
+		"image": "",
+		"name": "Adm Beira Rio",
+		"text": "bairro: beira rio<br/>rua: ...<br/>n⁰: ...}"
+	},
+	"mucurizinho": {
+		"image": "",
+		"name": "Adm Mucurizinho",
+		"text": "com: mucurizinho"
+	},
+	"nova canaã": {
+		"image": "",
+		"name": "Adm Nova canaã",
+		"text": "faz: nova canaã"
+	},
+	"ferreira": {
+		"image": "",
+		"name": "Adm Ferreira",
+		"text": "com: ferreira"
+	},
+	"nova esperança": {
+		"image": "",
+		"name": "Adm Nova esperança",
+		"text": "faz: nova esperança<br/>centro"
+	},
+	"paulo freire": {
+		"image": "",
+		"name": "Adm paulo freire",
+		"text": "Ass: paulo freire<br/>&nbsp&nbsp&nbspcentro"
+	},
+	"petruza": {
+		"image": "",
+		"name": "Adm petruza",
+		"text": "com: petruza<br/>&nbsp&nbsp&nbspcentro"
+	},
+	"jequitibá": {
+		"image": "img/icon.png",
+		"name": "Adm Jequitibá",
+		"text": "Ass: jequitibá<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspnúcleo: 5<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspcentro"
+	}
+};
+
 
 const ieadem = {
     "select": {
@@ -5,9 +63,372 @@ const ieadem = {
         "text": "<img source src='img/logo.png' width='20%' height='40%'>"
     },
     "sede": {
-        "name": "Sede",
-        "text": "<img source src='img/logo.png' width='20%' height='40%'>"
+    	 "obreiros": {
+    	 	"Filemon": {
+    	   	"image": "img/bal.png",
+    	   	"name": "Filemon",
+    	   	"function": "Pastor presidente"
+    	   },
+    	   "bal": {
+    	   	"image": "img/bal.png",
+    	   	"name": "",
+    	   	"function": ""
+    	   },
+    	   "Vilmar": {
+    	   	"image": "img/vilmar.png",
+    	   	"name": "Vilmar júnior araújo dos santos",
+    	   	"function": ""
+    	   },
+    	   "Jean": {
+    	   	"image": "img/jean.png",
+    	   	"name": "Jean Silva",
+    	   	"function": ""
+    	   },
+    	   "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   },
+    	   "....": {
+    	   	"image": "",
+    	   	"name": "....",
+    	   	"function": ""
+    	   },
+    	   ".....": {
+    	   	"image": "",
+    	   	"name": ".....",
+    	   	"function": ""
+    	   }
+    	 },
+    	 "lideres": {
+    	 	"...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "Luíza",
+    	   	"function": ""
+    	   }
+    	 }
     },
+    "califórnia": {
+      "obreiros": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      },
+      "lideres": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "brisas do mar": {
+      "obreiros": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   }
+      },
+      "lideres": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "beira rio": {
+      "obreiros": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      },
+      "lideres": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "mucurizinho": {
+      "obreiros": {
+      	 "Vilmar": {
+    	   	"image": "img/vilmar.png",
+    	   	"name": "Vilmar júnior araújo dos santos",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      },
+      "lideres": {
+      	 "Íta": {
+    	   	"image": "img/íta.png",
+    	   	"name": "Íta cristina",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "nova canaã": {
+      "obreiros": {
+      	 "Carlito": {
+    	   	"image": "img/carlito.png",
+    	   	"name": "Carlito",
+    	   	"function": "Pastor dirigente"
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      },
+      "lideres": {
+      	 "Celma": {
+    	   	"image": "img/celma.png",
+    	   	"name": "Celma",
+    	   	"function": "Missionária, dirigente do círculo de oração"
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "ferreira": {
+      "obreiros": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      },
+      "lideres": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "nova esperança": {
+      "obreiros": {
+      	 "Anivaldo": {
+    	   	"image": "",
+    	   	"name": "Anivaldo",
+    	   	"function": "presibítero dirigente"
+    	   },
+    	   "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   }
+      },
+      "lideres": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "paulo freire": {
+      "obreiros": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      },
+      "lideres": {
+      	 "..": {
+    	   	"image": "",
+    	   	"name": "..",
+    	   	"function": ""
+    	   },
+    	   "...": {
+    	   	"image": "",
+    	   	"name": "...",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "petruza": {
+      "obreiros": {
+      	 "Otôniel": {
+    	   	"image": "img/otôniel.jpg",
+    	   	"name": "Otôniel penha de paula",
+    	   	"function": "presibítero dirigente, tesoureiro"
+    	   },
+    	   "Nelson": {
+    	   	"image": "img/nelson.png",
+    	   	"name": "Nelson",
+    	   	"function": "..."
+    	   }
+      },
+      "lideres": {
+      	 "": {
+    	   	"image": "",
+    	   	"name": "",
+    	   	"function": ""
+    	   },
+    	   "": {
+    	   	"image": "",
+    	   	"name": "",
+    	   	"function": ""
+    	   }
+      }
+    },
+    "jequitibá": {
+    	 "obreiros": {
+    	   "Otôniel": {
+    	   	"image": "img/otôniel.jpg",
+    	   	"name": "Otôniel penha de paula",
+    	   	"function": "presibítero dirigente e<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspprofessor(EBD)"
+    	   },
+    	   "Euzito": {
+    	   	"image": "img/euzito.png",
+    	   	"name": "Euzito santos",
+    	   	"function": "presibítero e segundo dirigente"
+    	   },
+    	   "Alcides": {
+    	   	"image": "img/alcides.png",
+    	   	"name": "Alcides",
+    	   	"function": "presibítero"
+    	   },
+    	   "Dijalma": {
+    	   	"image": "img/dijalma.png",
+    	   	"name": "Dijalma maria",
+    	   	"function": "presibítero"
+    	   },
+    	   "Sebastião": {
+    	   	"image": "img/sebastião.png",
+    	   	"name": "Sebastião",
+    	   	"function": "presibítero"
+    	   },
+    	   "Manoel": {
+    	   	"image": "img/manoel.png",
+    	   	"name": "Manoel joão costa cinza",
+    	   	"function": "porteiro"
+    	   },
+        "ueder": {
+        	"image": "img/samods.jpg",
+        	"name": "ueder de souza avelar",
+        	"function": "coperador<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp sonoplasta e<br/>&nbsp&nbsp&nbsp&nbsp&nbsp zelador"
+        }
+      },
+      "lideres": {
+        "Fernanda": {
+    	   	"image": "img/fernanda.png",
+    	   	"name": "Fernanda lacerda",
+    	   	"function": "dirigente do círculo de oração"
+    	   },
+    	   "Ivanir": {
+    	   	"image": "img/ivanir.png",
+    	   	"name": "Ivanir lucas santos",
+    	   	"function": "segunda dirigente do círculo de oração e<br/>professora(EBD)"
+    	   },
+        "jucimara": {
+          "image": "img/jucimara.png",
+          "name": "Jucimara de souza avelar",
+          "function": "regente do círculo<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp de oração e<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp zeladora"
+        },
+        "Maria josé": {
+    	   	"image": "img/zezé.png",
+    	   	"name": "Maria josé pereira cinza",
+    	   	"function": "tesoureira do círculo de oração"
+    	   },
+    	   "Izabel": {
+    	   	"image": "img/izabel.png",
+    	   	"name": "Izabel cesário cristina da silva",
+    	   	"function": "secretária do círculo de oração"
+    	   },
+    	   "Lílian": {
+    	   	"image": "img/lílian.jpg",
+    	   	"name": "Lílian costa de Jesus",
+    	   	"function": "professora(EBD)"
+    	   },
+    	   "Rayssa": {
+    	   	"image": "img/rayssa.png",
+    	   	"name": "Rayssa lacerda evangelista",
+    	   	"function": "tesoureira"
+    	   }
+      }
+    }};
+    /*
     "brisas do mar": {
         "name": "Brisas do mar",
         "text": "<img source src='img/logo.png' width='20%' height='40%'>"
@@ -58,7 +479,8 @@ const ieadem = {
 	 </div>
 	 <br/>
 	 
-   	<h3>Obreiros, líderes e auxiliares</h3>
+   	<details>
+   	<summary>Obreiros</summary>
    	
    	<br/>
    	<div class="container">
@@ -110,7 +532,7 @@ const ieadem = {
 	 			<span></span>
 	 			<span></span>
 	 			<div class="text">
-	 				<h4>Alcides afonso de souza</h4>
+	 				<h4>Alcides</h4>
 	 				<p>presibítero e</p>
 	 				<p>professor(EBD)</p>
 	 			</div>
@@ -165,7 +587,7 @@ const ieadem = {
 	 			<span></span>
 	 			<span></span>
 	 			<div class="text">
-	 				<h4>Manoel joão</h4>
+	 				<h4>Manoel joão costa cinza</h4>
 	 				<p>porteiro</p>
 	 			</div>
 	 		</div>
@@ -191,7 +613,10 @@ const ieadem = {
 	 		   </div>
    		</div>
    	</div>
+	 </details>
 	 
+	 <details>
+   	<summary>Líderes e auxiliares</summary>
 	 <div class="container">
 	 	<div class="card">
 	 		<image source src="img/fernanda.png" class="self"/>
@@ -244,7 +669,7 @@ const ieadem = {
 	 			<div class="text">
 	 				<h4>Jucimara de souza avelar</h4>
 	 				<p>regente do</p>
-	 				<p>cículo de oração e</p>
+	 				<p>círculo de oração e</p>
 	 				<p>zeladora</p>
 	 			</div>
 	 		</div>
@@ -291,7 +716,7 @@ const ieadem = {
    		
    	<div class="container">
 	 	<div class="card">
-	 		<image source src="img/lilian.png" class="self"/>
+	 		<image source src="img/lílian.jpg" class="self"/>
 	 		<div class="layer">
 	 			<span></span>
 	 			<span></span>
@@ -319,30 +744,125 @@ const ieadem = {
 	 			<span></span>
 	 			<div class="text">
 	 				<h4>Rayssa lacerda evangelista</h4>
-	 				<p>secretária geral</p>
+	 				<p>tesoureira</p>
 	 			</div>
 	 		</div>
 	 	</div>
-	 </div>`
+	 </div>
+	 </details>`
     }
-};
+};*/
 
 
 window.onload = function(){
     let list = document.getElementById("data");
-    let div_text = document.getElementById("text");
-    // Remove a opção "Carregando..." da lista
+    let div_text = document.getElementById("object");
+    let div_text2 = document.getElementById("object2");
+    let congregacao = document.getElementById("congregacao");
+    // Remove a opÃ§Ã£o "Carregando..." da lista
     list.innerHTML = "";
+    // Adiciona cada elemento contido no objeto com as informaÃ§Ãµes
     
     for (key in ieadem) {
-		  list.innerHTML += `<option value="${key}">${ieadem[key]["name"]}</option>`;
-	 }
-	  
+    	   if (key == "select"){
+		      list.innerHTML += `<option value="${key}">${ieadem[key]["name"]}</option>`;
+	      }else{
+	      	list.innerHTML += `<option value="${key}">${key}</option>`;
+	      };
+	  }
     div_text.innerHTML = ieadem.select.text;
+    div_text2.innerHTML = ieadem.select.text;
     
+    document.getElementById("copyright").innerHTML = `© IEADEM - ${new Date().getFullYear()}`;
     
     list.onchange = function() {
-        let info = ieadem[this.value];
-        div_text.innerHTML = info.text;
-    }
+    	  div_text.innerHTML = "";
+    	  div_text2.innerHTML = "";
+    	  congregacao.innerHTML = "";
+       let info = ieadem[this.value];
+       let adm = adms[this.value];
+        
+       if (this.value !== "select"){
+          document.getElementById("dts").style.visibility = `visible`;
+          document.getElementById("dts").style.position = `relative`;
+          if (this.value == "petruza"){
+          	document.getElementById("dts2").style.visibility = `hidden`;
+             document.getElementById("dts2").style.position = `absolute`;
+          }else{
+               document.getElementById("dts2").style.visibility = `visible`;
+               document.getElementById("dts2").style.position = `relative`;
+          }
+       }else{
+        	document.getElementById("dts").style.visibility = `hidden`;
+          document.getElementById("dts").style.position = `absolute`;
+          document.getElementById("dts2").style.visibility = `hidden`;
+          document.getElementById("dts2").style.position = `absolute`;
+        };
+        
+       congregacao.innerHTML += `
+    		  <div class="container">
+	 	      <div class="card">
+	 		     <image source src="${adm['image']}"/>
+	 		       <div class="layer">
+	        			<span></span>
+	 	         		<span></span>
+	 	        		<span></span>
+	 	        		<span></span>
+	 	        		<span></span>
+	 	        		<span></span>
+	        			<div class="text">
+	 		        		<h4 style="color: #c30026;text-shadow: 5px 5px 5px #675654;">${adm["name"]}</h4>
+	        				<p>${adm["text"]}</p>
+	 		        	</div>
+	        		</div>
+        	 	</div>
+          </div>`;
+        
+        
+          for (key in info){		
+              for (item in info[key]){
+        	        if (key == "lideres"){
+        		
+        		         div_text2.innerHTML += `
+    		              <div class="container">
+	                     <div class="card">
+	 	                  	<image source src="${info[key][item]['image']}" class="self"/>
+	 	                  	<div class="layer">
+	 		                  	<span></span>
+	 		                  	<span></span>
+	 		                  	<span></span>
+	 		                  	<span></span>
+	                         <span></span>
+	                  			<span></span>
+	                  			<div class="text">
+	 			                  	<h4>${info[key][item]["name"]}</h4>
+	                  				<p>${info[key][item]["function"]}</p>
+	 	                  		</div>
+	                  		</div>
+	                     </div>
+                  	 </div>`;}
+        		
+        		      if (key == "obreiros"){
+    		              div_text.innerHTML += `
+    		               <div class="container">
+	                     <div class="card">
+	 	                  	<image source src="${info[key][item]['image']}" class="self"/>
+	 	                  	<div class="layer">
+	 		                  	<span></span>
+	 		                  	<span></span>
+	 		                  	<span></span>
+	 		                  	<span></span>
+	                         <span></span>
+	                  			<span></span>
+	                  			<div class="text">
+	 			                  	<h4>${info[key][item]["name"]}</h4>
+	                  				<p>${info[key][item]["function"]}</p>
+	 	                  		</div>
+	                  		</div>
+	                     </div>
+                  	 </div>`;
+                   }
+	            }
+      	   }
+      }
 };
